cd3fcfabdb21003881d76d000b1bb499b5c193b5
yjlim <yjlim@iconloop.com>
2020-02-12 15:06:13 +0900

    200212-test05

--- check/nginx_config.sh
+++ check/nginx_config.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+REPO_HUB = yjlim2
+NAME = nginx
+VERSION = 1.17.1
 
 
 docker exec -it nginx nginx -t
 if [ $? -eq 0 ]; then
   echo " >>>>>>>>>> NGINX Config Cheeck OK <<<<<<<<<< "
+  docker login -u="${DOCKER_USERNAME}" -p="${DOCKER_PASSWORD}"
+  docker push $(REPO_HUB)/$(NAME):$(TAGNAME)-dev
 
 else
   echo " !!!!!!!!!! NGINX Config Check Fail !!!!!!!!!! "
